f6aa0e10d49277fa52a874308f81f505edcacb8d
audri <lapin@et.esiea.fr>
2016-04-10 21:02:34 +0200

    lien vers fichier csv changé

--- EuroPokemon/src/test/java/euro_pokemon/EuroPokemon/ReaderEuroMillionTest.java
+++ EuroPokemon/src/test/java/euro_pokemon/EuroPokemon/ReaderEuroMillionTest.java
@@ -6,22 +6,12 @@ import ReaderCSV.ReaderEuroMillion;
 import junit.framework.TestCase;
 
 public class ReaderEuroMillionTest extends TestCase{
-	private final static String FILE_NAME = "src/test/resources/csv/euromillions_3.csv";
+	private final static String FILE_NAME = "resources/csv/euromillions_3.csv";
 	
 	public void testGetResourceCsvEuroMillion(){
-		// Param
-	    final String fileName = FILE_NAME;
-
-	    // Result
-	    // ...
-
-	    // Appel
-	    final File file = ReaderEuroMillion.getResource(fileName);
-
-	    // Test
-	    // On sait que le fichier existe bien puisque c'est avec lui qu'on travaille depuis le début.
-	    assertTrue(file.exists());
-		
+	   final String fileName = FILE_NAME;
+	   final File file = ReaderEuroMillion.getResource(fileName);
+	   assertTrue(file.exists());
 	}
 
 }
