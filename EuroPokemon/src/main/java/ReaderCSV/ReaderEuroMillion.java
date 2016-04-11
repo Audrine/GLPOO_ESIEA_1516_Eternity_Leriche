87959d57d02fad2da84164422446016887d9914a
TiTiaso <mathias.levostre@gmail.com>
2016-04-11 23:40:45 +0200

    Ajout des methodes : - de lecture du fichier csvEuroMillion - de tirage aléatoire
    Ajout de test 
    Suppression de la AppTest

--- EuroPokemon/src/main/java/ReaderCSV/ReaderEuroMillion.java
+++ EuroPokemon/src/main/java/ReaderCSV/ReaderEuroMillion.java
@@ -8,34 +8,44 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ReaderEuroMillion {
-	
-
 	public static String getResourcePath(String fileName){
 		final File f = new File("");
 	    final String dossierPath = f.getAbsolutePath() + File.separator + fileName;
 	    return dossierPath;
 	}
 	public static File getResource(String fileName) {
+			/**
+			 * Cette classe permet de récuperer le flux d'un fichier
+			 */
 	       final String completeFileName = getResourcePath(fileName);
 	       File file = new File(completeFileName);
 	       return file;
 	}
+	
 	public static List<String> readFile(File file) throws IOException {
+		/**
+		 * Class permetant de récupérer chaque ligne du fichier csv
+		 */
         List<String> result = new ArrayList<String>();
         FileReader fr = null;
 		try {
+			// lecture du fichier
 			fr = new FileReader(file);
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-        BufferedReader br = new BufferedReader(fr);
+		
+        BufferedReader br = new BufferedReader(fr);// lecture du flux du fichier
         for (String line = br.readLine(); line != null; line = br.readLine()) {
+        	// récupération ligne par ligne
             result.add(line);
         }
         try {
+        	// fermeture des fichiers 
 			br.close();
 			fr.close();
 		} catch (IOException e) {
@@ -44,6 +54,30 @@ public class ReaderEuroMillion {
 		}
         return result;
     }
+	
+	public static List<Integer> randTirage(List<String> listeLigneEuroMillion){
+		/**
+		 * Cette classe aura pour but de répurer les 5 numéros et 
+		 * les 2 numéros complémentaires au hasard
+		 * 
+		 */
+		 Random rand = new Random(); 
+		 int randLigne = rand.nextInt(229 - 2 + 1) + 2;
+		 List<Integer> resultat = new ArrayList<Integer>(); 
+		 // On prend un ligne au hasard
+		 String ligne = listeLigneEuroMillion.get(randLigne);
+		 
+		 // On découpe le ligne avec le séparateur ";"
+		 String[] splitLigne = ligne.split(";"); 
+		 
+		 for(int i = 4; i < 11; i++){
+			 //On récupère les 6 numéros
+			 resultat.add(Integer.parseInt(splitLigne[i])); 
+		 }
+		
+		return resultat;
+		
+	}
 	
 	
 }
--- EuroPokemon/src/test/java/euro_pokemon/EuroPokemon/ReaderEuroMillionTest.java
+++ EuroPokemon/src/test/java/euro_pokemon/EuroPokemon/ReaderEuroMillionTest.java
@@ -1,17 +1,38 @@
 package euro_pokemon.EuroPokemon;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import ReaderCSV.ReaderEuroMillion;
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class ReaderEuroMillionTest extends TestCase{
 	private final static String FILE_NAME = "resources/csv/euromillions_3.csv";
 	
 	public void testGetResourceCsvEuroMillion(){
-	   final String fileName = FILE_NAME;
-	   final File file = ReaderEuroMillion.getResource(fileName);
-	   assertTrue(file.exists());
+		final String fileName = FILE_NAME;
+		File file = ReaderEuroMillion.getResource(fileName);
+		assertTrue(file.exists());
 	}
+	
+	public void testReaderCsvEuroMillion() throws IOException{
+		final String fileName = FILE_NAME;
+		final File file = ReaderEuroMillion.getResource(fileName);
+		int nombreDeLigne = 229; 
+	    List<String> lines = ReaderEuroMillion.readFile(file);
+	    Assert.assertEquals(nombreDeLigne, lines.size());
+	}
+	
+	public void testrandTirage() throws IOException{
+		final String fileName = FILE_NAME;
+		final File file = ReaderEuroMillion.getResource(fileName);
+	    List<String> lines = ReaderEuroMillion.readFile(file);
+	    List<Integer> tirage = ReaderEuroMillion.randTirage(lines);
+	    assertTrue(!(tirage.isEmpty()));
+	}
+	
+	
 
 }
