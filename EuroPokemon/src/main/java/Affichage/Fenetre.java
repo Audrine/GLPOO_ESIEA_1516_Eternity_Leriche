986184aad737777611db5c2995e7146da6f43932
TiTiaso <TiTiaso@TiTiaso-Ok>
2016-04-13 13:07:05 +0200

    Déplacement Button achive !!!!!!!

--- EuroPokemon/src/main/java/Affichage/Fenetre.java
+++ EuroPokemon/src/main/java/Affichage/Fenetre.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Insets;
 import java.io.File;
 
 import javax.swing.ImageIcon;
@@ -21,6 +22,7 @@ public class Fenetre extends JFrame{
 	JButton jeu = new JButton("Jouer");
 	JButton aide = new JButton("Aide");
 	JButton quitter = new JButton("Quitter");
+	JPanel menuPrinciaple = new ImagePanel();
 	
 	public Fenetre() {
 		
@@ -28,25 +30,20 @@ public class Fenetre extends JFrame{
 		// Initialisation de la page
 		this.setTitle("EuroPokemon");
 		setSize(750, 500);
-	    this.setLayout(new FlowLayout()); 
 		//Definition du CardLayout
 		cardLayout = new JPanel();
 		cardLayout.setLayout(new CardLayout());
 		
 		
 		///////////////////Panel Menu Principal ///////////////
-		JPanel menuPrinciaple = new ImagePanel();
-		JPanel pan = new JPanel(null); 
-		jeu.setBounds(10, 11, 20, 20);
-		pan.add(jeu);
+		menuPrinciaple.add(jeu); 
+		Insets insets = menuPrinciaple.getInsets();
+		Dimension size = jeu.getPreferredSize();
+		jeu.setBounds(50 + insets.left, 11 +insets.top, size.width, size.height);
 		
-		//pan.add(aide);
-		//pan.add(quitter);
 		
-		cardLayout.add(NameMenuPrincipal, pan);
-		
+		cardLayout.add(NameMenuPrincipal, menuPrinciaple);
 		////////////////////FIN////////////////////////////////
-		setContentPane(menuPrinciaple);
 		add(cardLayout); 
 		
 	    
