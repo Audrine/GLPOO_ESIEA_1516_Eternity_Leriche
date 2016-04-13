93f672f42a3c7a9bc90f9ada4413c3e47b5380a6
TiTiaso <TiTiaso@TiTiaso-Ok>
2016-04-13 13:50:13 +0200

    Ajout du titre dans le menu principal

--- EuroPokemon/src/main/java/Affichage/Fenetre.java
+++ EuroPokemon/src/main/java/Affichage/Fenetre.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Insets;
 import java.io.File;
 
@@ -12,6 +13,8 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Fenetre extends JFrame{
 	final static String NameMenuPrincipal = "menu_principal"; 
@@ -23,7 +26,7 @@ public class Fenetre extends JFrame{
 	JButton aide = new JButton("Aide");
 	JButton quitter = new JButton("Quitter");
 	JPanel menuPrinciaple = new ImagePanel();
-	
+	JLabel titre = new JLabel("EuroPokemon");
 	public Fenetre() {
 		
 		 
@@ -36,11 +39,29 @@ public class Fenetre extends JFrame{
 		
 		
 		///////////////////Panel Menu Principal ///////////////
-		menuPrinciaple.add(jeu); 
+		menuPrinciaple.add(jeu);
+		menuPrinciaple.add(aide);
+		menuPrinciaple.add(quitter);
+		titre.setOpaque(false);
+		titre.setFont(new Font("Serif", Font.BOLD, 25));
+		menuPrinciaple.add(titre);
 		Insets insets = menuPrinciaple.getInsets();
-		Dimension size = jeu.getPreferredSize();
-		jeu.setBounds(50 + insets.left, 11 +insets.top, size.width, size.height);
 		
+		//Boutton jeu 
+		Dimension size = jeu.getPreferredSize();
+		jeu.setBounds(340 + insets.left, 250 +insets.top, size.width, size.height);
+		
+		
+		//Boutton aide 
+		size = aide.getPreferredSize();
+		aide.setBounds(344 + insets.left, 280 +insets.top, size.width, size.height);
+		
+		//Boutton aide 
+		size = quitter.getPreferredSize();
+		quitter.setBounds(336 + insets.left, 310 +insets.top, size.width, size.height);
+		
+		size = titre.getPreferredSize();
+		titre.setBounds(300 + insets.left, 60 +insets.top, size.width, size.height);
 		
 		cardLayout.add(NameMenuPrincipal, menuPrinciaple);
 		////////////////////FIN////////////////////////////////
